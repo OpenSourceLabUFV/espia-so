7506313440fb5709cb52f63ef9809823c56a22f0
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-03-22 22:56:54 -0300

    Change Javascript files to Typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,13 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
     <Layout>
-      <p class="text-xl max-w-screen-lg text-center"> 
+      <p className="text-xl max-w-screen-lg text-center"> 
         O Espia Só é um lugar que vai te ajudar a passar nas matérias de Ciência da Computação.
         É um espaço onde os próprios alunos colaboram com diversas informações, como: links úteis, resumos,
         materiais de anos anteriores e dicas.
       </p>
       <Link to="/disciplinas">
-      <button class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white mt-8">
+      <button className="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white mt-8">
         Disciplinas
       </button>
       </Link>
