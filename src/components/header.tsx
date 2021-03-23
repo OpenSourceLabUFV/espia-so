718d89533b832647558ededa1460c7a7641c5927
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-03-22 22:59:06 -0300

    Change components to Typescript

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+        <div className="flex items-center mb-8 content-center">
+                <img className="w-14 mr-4" src="https://svgshare.com/i/VET.svg" alt=""/>
+                <h1 className="text-4xl font-sans font-bold"> Espia Só </h1>
+            </div>
+        </header>
+    )
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import Header from '../components/header';
 
 export default function Layout({ children }) {
     return (
-        <div class="flex flex-col items-center content-center m-10 h-100">
+        <div className="flex flex-col items-center content-center m-10 h-100">
             <Header></Header>
             {children}
         </div>
