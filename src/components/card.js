384e90b8c6a347dd978b46423252310be1d19b92
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-04-17 18:23:49 -0300

    Fix unwanted word breaking

--- src/components/card.js
+++ src/components/card.js
@@ -10,7 +10,7 @@ export default class Card extends Component {
             <div className="m-6">
                 <Link to={this.props.slug}>
                     <div className="flex flex-col content-center justify-center h-full bg-gray-50 text-black rounded-md shadow-sm p-8 hover:shadow-md hover:text-white hover:bg-blue-800 origin-left transition all 1.0s ease">
-                        <h2 className="text-xl font-semibold">{this.props.name}</h2>
+                        <h2 className="text-xl font-semibold break-normal">{this.props.name}</h2>
                         <h3 className="mt-1">{this.props.code}</h3>
                     </div>
                 </Link>
