d343a95466b565a55a33718786268ee6e0f602dc
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-07-13 12:27:45 -0300

    Disciplines search

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,28 +1,57 @@
 <template>
   <div class="flex flex-col items-center mt-10 mb-0">
-    <g-link to="/">
     <header class="flex items-center content-center max-w-screen-md mb-8">
-      <g-image alt="Ícone Espia Só" src="~/favicon.png" width="80"/>
-      <strong>
-        <h1 class="text-4xl font-bold">{{ $static.metadata.siteName }}</h1>
-      </strong>
+      <g-link to="/">
+        <div class="flex flex-row items-center">
+          <g-image alt="Ícone Espia Só" src="~/favicon.png" width="80"/>
+          <strong>
+            <h1 class="ml-2 text-4xl font-bold">{{ $static.metadata.siteName }}</h1>
+          </strong>
+        </div>
+      </g-link>
+      <div class="relative pt-2 mx-8 text-gray-600">
+        <input class="h-10 px-5 pr-16 text-sm bg-white border-2 border-gray-300 rounded-lg focus:outline-none"
+          type="text" id="search" name="search" placeholder="Busque por uma Disciplina" v-model="search">
+      </div>
       <!-- <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav> -->
     </header>
-    </g-link>
-    <slot/>
+    <div v-if="searchResults.length > 0 && search.length > 0">
+        <div v-for="disc in searchResults" :key="disc.node.code">
+            <Card :name="disc.node.name" :code="disc.node.code" :url="$url('disciplinas/' + disc.node.code.toLowerCase())"/>
+        </div>
+    </div>
+    <div v-else>
+      <slot/>
+    </div>
     <Footer/>
   </div>
 </template>
 
 <script>
 import Footer from '../components/Footer.vue';
+import Card from '../components/Card.vue';
+
 export default {
   components: {
-    Footer
-  }
+    Footer,
+    Card
+  },
+  data() {
+        return {
+            search: ''
+        }
+    },
+    computed: {
+        searchResults() {
+            return this.$static.allDisciplinesRelation.edges.filter(discipline => {
+                const term = this.search.toLowerCase().trim();
+                return discipline.node.name.toLowerCase().includes(term) || discipline.node.code.toLowerCase().includes(term)
+            })
+        }
+    }
 }
 </script>
 
@@ -30,6 +59,14 @@ export default {
 query {
   metadata {
     siteName
+  }
+  allDisciplinesRelation {
+    edges {
+      node {
+        code
+        name
+      }
+    }
   }
 }
 </static-query>
