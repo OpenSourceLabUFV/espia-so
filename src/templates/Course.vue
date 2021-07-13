f8123a6c719e9b5e58115f448cfd701d7bcb6809
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-07-13 11:16:47 -0300

    Fix disciplines links

--- src/templates/Course.vue
+++ src/templates/Course.vue
@@ -3,7 +3,7 @@
         <h1 class="text-xl font-semibold">{{ $context.title }}</h1>
         <div class="grid items-center grid-flow-row grid-cols-1 auto-rows-max lg:grid-cols-4">
             <div v-for="disc in $page.disciplines.edges" :key="disc.node.code">
-                <Card :name="disc.node.name" :code="disc.node.code" :url="$url('disciplinas/' + disc.node.code)"></Card>
+                <Card :name="disc.node.name" :code="disc.node.code" :url="$url('disciplinas/' + disc.node.code.toLowerCase())"></Card>
             </div>
         </div>
     </Layout>
